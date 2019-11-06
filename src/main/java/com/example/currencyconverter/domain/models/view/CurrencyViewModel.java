f668e5a57543c3f9a8bff1be6fbac9cbb2c3a3ae
Plamen <plamen.borachev@gmail.com>
2019-11-06 19:39:05 +0200

    Convert Currency page currency selection made with select menu

---
+++ src/main/java/com/example/currencyconverter/domain/models/view/CurrencyViewModel.java
@@ -0,0 +1,26 @@
+package com.example.currencyconverter.domain.models.view;
+
+public class CurrencyViewModel {
+
+    private String id;
+    private String name;
+
+    public CurrencyViewModel() {
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/com/example/currencyconverter/web/controllers/CurrencyController.java
+++ src/main/java/com/example/currencyconverter/web/controllers/CurrencyController.java
@@ -3,6 +3,7 @@ package com.example.currencyconverter.web.controllers;
 import com.example.currencyconverter.domain.models.binding.ConverterBindingModel;
 import com.example.currencyconverter.domain.models.binding.CurrencyAddBindingModel;
 import com.example.currencyconverter.domain.models.service.CurrencyServiceModel;
+import com.example.currencyconverter.domain.models.view.CurrencyViewModel;
 import com.example.currencyconverter.error.CurrencyNameAlreadyExistsException;
 import com.example.currencyconverter.service.CurrencyService;
 import com.example.currencyconverter.validation.CurrencyAddValidator;
@@ -12,6 +13,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/currencies")
@@ -76,6 +80,15 @@ public class CurrencyController {
         modelAndView.addObject("model", model);
         modelAndView.setViewName("/currencies/convert-currency-result");
         return modelAndView;
+    }
+
+    @GetMapping("/fetch")
+    @ResponseBody
+    public List<CurrencyViewModel> fetchCurrencies() {
+        return this.currencyService.findAllCurrencies()
+                .stream()
+                .map(c -> this.modelMapper.map(c, CurrencyViewModel.class))
+                .collect(Collectors.toList());
     }
 
 
