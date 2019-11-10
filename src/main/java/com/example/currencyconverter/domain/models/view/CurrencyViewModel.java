c178ee6a5b2497cb9daa45ca0614cee39a640708
Plamen <plamen.borachev@gmail.com>
2019-11-10 13:12:22 +0200

    Convert calculations made without reloading the page by AJAX

--- src/main/java/com/example/currencyconverter/domain/models/view/CurrencyViewModel.java
+++ src/main/java/com/example/currencyconverter/domain/models/view/CurrencyViewModel.java
@@ -1,9 +1,13 @@
 package com.example.currencyconverter.domain.models.view;
 
+import java.math.BigDecimal;
+
 public class CurrencyViewModel {
 
     private String id;
     private String name;
+    private BigDecimal perUnitOfCurrency;
+    private BigDecimal rate;
 
     public CurrencyViewModel() {
     }
@@ -22,5 +26,21 @@ public class CurrencyViewModel {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public BigDecimal getPerUnitOfCurrency() {
+        return this.perUnitOfCurrency;
+    }
+
+    public void setPerUnitOfCurrency(BigDecimal perUnitOfCurrency) {
+        this.perUnitOfCurrency = perUnitOfCurrency;
+    }
+
+    public BigDecimal getRate() {
+        return this.rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
     }
 }
